b3af4ace330e04031cc5c4b4bb60c122dd9a66ce
lucas mouta <lucasaraujomouta@gmail.com>
2021-04-06 02:19:15 -0300

    finalizing the project

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,66 +1,59 @@
 class TasksController < ApplicationController
+  include Exportable
   before_action :set_task, only: %i[ edit update destroy ]
 
-  # GET /tasks or /tasks.json
+  
   def index
-    @tasks = Task.order(due_date: :desc, description:)
+    @tasks = Task.only_parents.order(:due_date)
   end
 
-  # GET /tasks/new
+  
   def new
     @task = Task.new
   end
 
-  # GET /tasks/1/edit
+  
   def edit
   end
 
-  # POST /tasks or /tasks.json
+  
   def create
     @task = Task.new(task_params)
 
-    respond_to do |format|
-      if @task.save
-        format.html { redirect_to tasks_path, notice: "Tarefa criada com sucesso." }
-        format.json { render :show, status: :created, location: @task }
-      else
-        flash.now[:alert] = @task.errors.full_messages.to_sentence
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+    if @task.save
+      redirect_to tasks_path, notice: "Tarefa criada com sucesso."
+    else
+      flash.now[:alert] = @task.errors.full_messages.to_sentence
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /tasks/1 or /tasks/1.json
+
   def update
-    respond_to do |format|
-      if @task.update(task_params)
-        format.html { redirect_to tasks_path, notice: "Tarefa atualizada com sucesso." }
-        format.json { render :show, status: :ok, location: @task }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+    
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "Tarefa atualizada com sucesso."
+    else
+      flash.now[:alert] = @task.errors.full_messages.to_sentence
+      render :edit, status: :unprocessable_entity 
     end
   end
 
-  # DELETE /tasks/1 or /tasks/1.json
+  
   def destroy
     @task.destroy
-    respond_to do |format|
-      format.html { redirect_to tasks_url, notice: "Tarefa removida com sucesso" }
-      format.json { head :no_content }
-    end
+      redirect_to tasks_url, notice: "Tarefa removida com sucesso" 
   end
 
+
   private
-    # Use callbacks to share common setup or constraints between actions.
+   
     def set_task
       @task = Task.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
+   
     def task_params
-      params.require(:task).permit(:description, :due_date, :done)
+      params.require(:task).permit(:description, :due_date, :done, :parent_id)
     end
 end
